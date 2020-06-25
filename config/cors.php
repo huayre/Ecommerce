4717e9085edef7d9211afe19b92c21fc9086331f
HuayreBujaico <roy.huayre@unas.edu.pe>
2020-06-25 12:35:51 -0500

    mejora

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*','web/*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ToXVic</title>
     <link rel="icon" href="{{secure_asset('logo.png')}}" type="image/icon type" class="img-circle">
     <!-- Tell the browser to be responsive to screen width -->
@@ -259,3 +260,4 @@
 @yield('script')
 </body>
 </html>
+
