960f6210d0ba2e470d9e842752ffa92d8fa22c1a
HuayreBujaico <roy.huayre@unas.edu.pe>
2020-06-25 12:50:47 -0500

    mejora2

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -1,9 +1,8 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ToXVic</title>
     <link rel="icon" href="{{secure_asset('logo.png')}}" type="image/icon type" class="img-circle">
     <!-- Tell the browser to be responsive to screen width -->
