3dba4958e5e7db953c4aa90c75b60b2432c39eb2
HuayreBujaico <roy.huayre@unas.edu.pe>
2020-06-26 00:26:41 -0500

    message

--- resources/views/layouts/portada/header.blade.php
+++ resources/views/layouts/portada/header.blade.php
@@ -1,19 +1,23 @@
-
-    <div class="p-3 " style="background:#f4fb39">
-        <div class="float-right mr-3 mt-1">
-            <h5 class="d-inline mr-5"><i class="fas fa-phone-alt"></i> 929792508</h5>
-            <h5 class="d-inline">Visítanos en:</h5>
-            <a href="https://www.instagram.com/?hl=es-la" target="_blank"><i class="ml-1 fab fa-instagram" style="color:#dd2a7b" ></i></a>
-            <a href="https://www.facebook.com/toxvic/?modal=admin_todo_tour" target="_blank"><i class="ml-1 fab fa-facebook" style="color:#3b5998"></i></a>
-        </div>
-        <div>
+<link href="https://fonts.googleapis.com/css2?family=Damion&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet">
+    <div class="p-3 row " style="background:#f4fb39">
+        <div class="col" >
              <a href="{{route('inicio')}}">
-                 <img src="{{asset('logov2.png')}}" width="150px">
+                 <img src="{{asset('logov2.png')}}" width="150px" class="ml-3">
              </a>
+        </div>
+        <div class="col">
+            <h3 id="textnacional" class="text-center">Envios A Nivel Nacional</h3>
+        </div>
+        <div class="col d-flex justify-content-end">
+            <p>visítanos</p>
+            <a href="https://www.instagram.com/?hl=es-la" target="_blank"><i class=" fab fa-instagram mx-1" style="color:#dd2a7b" ></i></a>
+            <a href="https://www.facebook.com/toxvic/?modal=admin_todo_tour" target="_blank"><i class=" fab fa-facebook" style="color:#3b5998"></i></a>
         </div>
     </div>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
         <div class="container-xl">
             <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,10 +25,10 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav mr-auto" id="tamaño">
                     @foreach($ListCategory as $cat)
                         @if($cat->subcategories->count()>0)
-                            <li class="nav-item dropdown navbar-item has-dropdown is-hoverable">
+                            <li class="nav-item dropdown navbar-item has-dropdown is-hoverable text-uppercase">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{$cat->name}}
                                     <i class="fas fa-angle-down text-danger"></i>
