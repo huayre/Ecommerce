114ddf45ab850d73479971a63c4b4db48929f4bb
HuayreBujaico <roy.huayre@unas.edu.pe>
2020-07-01 09:02:42 -0500

    messenger

--- resources/views/layouts/portada/index.blade.php
+++ resources/views/layouts/portada/index.blade.php
@@ -21,31 +21,7 @@
              @include('layouts.portada.header')
              @include('layouts.portada.carrusel')
              @yield('contenido')
-             <!-- Load Facebook SDK for JavaScript -->
-                 <div id="fb-root"></div>
-                 <script>
-                     window.fbAsyncInit = function() {
-                         FB.init({
-                             xfbml            : true,
-                             version          : 'v7.0'
-                         });
-                     };
-
-                     (function(d, s, id) {
-                         var js, fjs = d.getElementsByTagName(s)[0];
-                         if (d.getElementById(id)) return;
-                         js = d.createElement(s); js.id = id;
-                         js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
-                         fjs.parentNode.insertBefore(js, fjs);
-                     }(document, 'script', 'facebook-jssdk'));</script>
-
-                 <!-- Your Chat Plugin code -->
-                 <div class="fb-customerchat"
-                      attribution=setup_tool
-                      page_id="100772375039619"
-                      logged_in_greeting="Hola! ¿Comó  podemos ayudarte?"
-                      logged_out_greeting="Hola! ¿Comó  podemos ayudarte?">
-                 </div>
+              @include('layouts.portada.chatredessociales')
              @include('layouts.portada.footer')
          </div>
 
