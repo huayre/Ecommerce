83a244a3c03f4877da4a2757c01e8567c6a0ffff
HuayreBujaico <roy.huayre@unas.edu.pe>
2020-07-03 12:08:49 -0500

    mejora carrusel

--- resources/views/App/Frond/welcome.blade.php
+++ resources/views/App/Frond/welcome.blade.php
@@ -4,161 +4,107 @@
 @endsection
 @section('contenido')
 <div style="height: 10px;background:#f4fb39"></div>
+<div class="mt-5">
 
-    <div class="container text-center my-3 mt-5">
-        <h2 class="font-weight-light text-primary"><i class="fas fa-shopping-bag text-danger"></i><strong> Los modelos más vendidos de la temporada </strong><i class="fas fa-shopping-bag text-danger"></i></h2>
-        <div class="row mx-auto my-auto">
-            <div id="recipeCarousel" class="carousel carousel1 slide w-100"  data-ride="carousel">
-                <div class="carousel-inner w-100 " id="carousel-inner" role="listbox">
-                    @foreach($ListArticlesMasComprados as $art)
-                        <div class="carousel-item @if($loop->index==0)active @endif">
-                              @foreach($art->photos as $p)
-                                <div class="col-md-4">
-                                    <div class="card shadow m-2">
-                                            @if($loop->index==0)
-                                                <a href="{{route('detalleproducto',$art->id)}}">
-                                                    <img src="{{$p->url}}" class="card-img-top" height="270px">
-                                                </a>
-                                            @endif
-                                                <div class="card-body card-body-cascade text-center p-2">
-                                                    <p class="card-text font-weight-lighter"><strong>{{$art->model}}</strong></p>
-                                                    <!--Rating-->
+    <h2 class="font-weight-light text-primary text-center"><i class="fas fa-shopping-bag text-danger"></i><strong> Los modelos más vendidos de la temporada </strong><i class="fas fa-shopping-bag text-danger"></i></h2>
 
-                                                    <ul class="nav d-flex justify-content-center mb-1">
-                                                        <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                        <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                        <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                        <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                        <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                    </ul>
-                                                    <p class="card-text font-weight-lighter h4"><strong>S/. {{$art->price}}</strong></p>
+    <div class="owl-carousel owl-theme">
+        @foreach($ListArticlesMasComprados as $art)
+            <div class="item">
+                @foreach($art->photos as $p)
+                    <div class="d-block ">
+                        <div class="card shadow m-2">
+                            @if($loop->index==0)
+                                <a href="{{route('detalleproducto',$art->id)}}">
+                                    <img src="{{$p->url}}" class="card-img-top" height="270px">
+                                </a>
+                            @endif
+                            <div class="card-body card-body-cascade text-center p-2">
+                                <p class="card-text font-weight-lighter"><strong>{{$art->model}}</strong></p>
+                                <!--Rating-->
+
+                                <ul class="nav d-flex justify-content-center mb-1">
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                </ul>
+                                <p class="card-text font-weight-lighter h4"><strong>S/. {{$art->price}}</strong></p>
 
 
-                                                </div>
+                            </div>
 
-                                    </div>
-                                </div>
-                               @endforeach
                         </div>
-                    @endforeach
-
-                </div>
-                <a class="carousel-control-prev w-auto" href="#recipeCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next w-auto" href="#recipeCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                    </div>
+                @endforeach
             </div>
-        </div>
+        @endforeach
     </div>
+    <h2 class="font-weight-light text-primary text-center"><i class="fas fa-shopping-bag text-danger"></i><strong> Nuevos modelos </strong><i class="fas fa-shopping-bag text-danger"></i></h2>
 
-    <div class="container text-center my-3 mt-5">
-        <h2 class="font-weight-light text-primary"><i class="fas fa-shopping-bag text-danger"></i><strong> Nuevos modelos </strong><i class="fas fa-shopping-bag text-danger"></i></h2>
-        <div class="row mx-auto my-auto">
-            <div id="recipeCarousel1" class="carousel carousel1 slide w-100"  data-ride="carousel">
-                <div class="carousel-inner w-100 " id="carousel-inner" role="listbox">
-                    @foreach($ListArticlesMasComprados as $art)
-                        <div class="carousel-item @if($loop->index==0)active @endif">
-                            @foreach($art->photos as $p)
-                                <div class="col-md-4">
-                                    <div class="card shadow m-2">
-                                        @if($loop->index==0)
-                                            <a href="{{route('detalleproducto',$art->id)}}">
-                                                <img src="{{$p->url}}" class="card-img-top" height="270px">
-                                            </a>
-                                        @endif
-                                        <div class="card-body card-body-cascade text-center p-2">
-                                            <p class="card-text font-weight-lighter"><strong>{{$art->model}}</strong></p>
-                                            <!--Rating-->
+    <div class="owl-carousel owl-theme">
+        @foreach($ListArticlesMasComprados as $art)
+            <div class="item">
+                @foreach($art->photos as $p)
+                    <div class="d-block ">
+                        <div class="card shadow m-2">
+                            @if($loop->index==0)
+                                <a href="{{route('detalleproducto',$art->id)}}">
+                                    <img src="{{$p->url}}" class="card-img-top" height="270px">
+                                </a>
+                            @endif
+                            <div class="card-body card-body-cascade text-center p-2">
+                                <p class="card-text font-weight-lighter"><strong>{{$art->model}}</strong></p>
+                                <!--Rating-->
 
-                                            <ul class="nav d-flex justify-content-center mb-1">
-                                                <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                                <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
-                                            </ul>
-                                            <p class="card-text font-weight-lighter h4"><strong>S/. {{$art->price}}</strong></p>
+                                <ul class="nav d-flex justify-content-center mb-1">
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                    <li class="nav-item"><i class="fas fa-star text-warning"></i></li>
+                                </ul>
+                                <p class="card-text font-weight-lighter h4"><strong>S/. {{$art->price}}</strong></p>
 
 
-                                        </div>
+                            </div>
 
-                                    </div>
-                                </div>
-                            @endforeach
                         </div>
-                    @endforeach
-
-                </div>
-                <a class="carousel-control-prev w-auto" href="#recipeCarousel1" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next w-auto" href="#recipeCarousel1" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                    </div>
+                @endforeach
             </div>
-        </div>
+        @endforeach
     </div>
-    <div class="row mx-4 my-5">
-        <div class="col-md-6">
-            <img class="w-100 rounded shadow " src="{{asset('images/varones.jpg')}}" height="500">
-        </div>
-        <div class="col-md-6">
-
-        </div>
-    </div>
-    <div class="row mx-4 my-5">
-        <div class="col-md-6">
-
-        </div>
-        <div class="col-md-6">
-            <img class="w-100 rounded shadow " src="{{asset('images/varones.jpg')}}" height="500">
-        </div>
-
-    </div>
-    <div class="row mx-4 my-5">
-        <div class="col-md-6">
-            <img class="w-100 rounded shadow " src="{{asset('images/varones.jpg')}}" height="500">
-        </div>
-        <div class="col-md-6">
-
-        </div>
-    </div>
-
-
-
+</div>
 @endsection
 @section('script')
+
     <script>
-        $('#recipeCarousel').carousel({
-            interval: 1500
-        })
-        $('#recipeCarousel1').carousel({
-            interval: 1500
-        })
-
-        $('.carousel1 .carousel-item').each(function(){
-            var minPerSlide = 3;
-            var next = $(this).next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-
-            for (var i=0;i<minPerSlide;i++) {
-                next=next.next();
-                if (!next.length) {
-                    next = $(this).siblings(':first');
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:7,
+            nav:true,
+            autoplay:true,
+            autoplayTimeout:1500,
+            responsive:{
+                0:{
+                    items:1
+                },
+                576:{
+                    items:2
+                },
+                700:{
+                    items:3
+                },
+                1100:{
+                    items:4
+                },
+                1200:{
+                    items:5
                 }
-
-                next.children(':first-child').clone().appendTo($(this));
             }
-        });
+        })
     </script>
 
 @endsection
--- resources/views/layouts/portada/index.blade.php
+++ resources/views/layouts/portada/index.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="{{asset('template_admin/plugins/fontawesome-free/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/frond.css')}}">
 
+    <link rel="stylesheet" href="{{asset('packages/owl_carousel/owl.carousel.css')}}">
     <title>Hello, world!</title>
 
     @yield('styles')
@@ -34,6 +35,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="{{asset('bootstrap4.5/js/bootstrap.js')}}"></script>
+<script src="{{asset('packages/owl_carousel/owl.carousel.js')}}"></script>
 @yield('script')
 
 </body>
